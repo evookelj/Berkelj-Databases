ef490087408490a1c804d0cb93c369d0871fd2f6
eVookelj <evukelj@stuy.edu>
2016-10-15 11:58:11 -0400

    Implements CSV->dict->table

--- csv_tester.py
+++ csv_tester.py
@@ -4,8 +4,4 @@ fObj = open("peeps.csv")
 d=csv.DictReader(fObj)
 
 for k in d:
-    print k
-    #Q: What can you print here to make each line show only
-    #   a name and its ID?
-    #   eg,
-    #   sasha, 3
+    print "%s, %s" % (k['name'],k['id'])
--- db_builder.py
+++ db_builder.py
@@ -1,23 +1,26 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-
 f="discobandit.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
-#...perhaps by beginning with these examples...
-
-'''
+#==========PEEP CREATION===========
+csvPeeps = csv.DictReader(open("peeps.csv"))
 q = "CREATE TABLE students (name TEXT, id INTEGER)"
-c.execute(q)    #run SQL query
+c.execute(q)
+for peep in csvPeeps:
+    q = "INSERT INTO students VALUES (\"%s\", %d)" % (peep['name'],int(peep['id']))
+    c.execute(q)
+
+#==========COURSES CREATION===========
+csvCourses = csv.DictReader(open("courses.csv"))
 q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
 c.execute(q)
-'''
+for course in csvCourses:
+    q = "INSERT INTO courses VALUES (\"%s\", %d, %d)" % (course['code'],int(course['mark']),int(course['id']))
+    c.execute(q)
 
-#==========================================================
 db.commit() #save changes
 db.close()  #close database
