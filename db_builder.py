0883ed303f671fe70459b81c3f65300ff07285bf
eVookelj <evukelj@stuy.edu>
2016-10-15 12:07:00 -0400

    Get rid of error when table exists + add explanatory print statements

--- db_builder.py
+++ db_builder.py
@@ -8,19 +8,28 @@ c = db.cursor()    #facilitate db ops
 
 #==========PEEP CREATION===========
 dPeeps = csv.DictReader(open("peeps.csv"))
-q = "CREATE TABLE students (name TEXT, id INTEGER)"
-c.execute(q)
-for peep in dPeeps:
-    q = "INSERT INTO students VALUES (\"%s\", %d)" % (peep['name'],int(peep['id']))
+try:
+    q = "CREATE TABLE students (name TEXT, id INTEGER)"
     c.execute(q)
+    for peep in dPeeps:
+        q = "INSERT INTO students VALUES (\"%s\", %d)" % (peep['name'],int(peep['id']))
+        c.execute(q)
+    print "TABLE students successfully populated with contents of 'peeps.csv'"
+except:
+    print "Error: TABLE 'students' already exists. Cannot re-create. Delete %s and re-run this file to recreate table.\n" % (f)
 
 #==========COURSES CREATION===========
 dCourses = csv.DictReader(open("courses.csv"))
-q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
-c.execute(q)
-for course in dCourses:
-    q = "INSERT INTO courses VALUES (\"%s\", %d, %d)" % (course['code'],int(course['mark']),int(course['id']))
+try:
+    q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
     c.execute(q)
+    for course in dCourses:
+        q = "INSERT INTO courses VALUES (\"%s\", %d, %d)" % (course['code'],int(course['mark']),int(course['id']))
+        c.execute(q)
+    print "TABLE courses successfully populated with contents of 'courses.csv'"
+
+except:
+    print "Error: TABLE 'courses' already exists. Cannot re-create. Delete %s and re-run this file to recreate table.\n" % (f)
 
 db.commit() #save changes
 db.close()  #close database
