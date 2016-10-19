ef6cae74d5fcfc53190f40135b00aa5512cabaf5
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-19 13:43:08 -0400

    Run side-effects in if _name__='__main__'... block

--- get_averages.py
+++ get_averages.py
@@ -2,14 +2,9 @@ import sqlite3
 import csv
 from numpy import mean
 
-f = "discobandit.db"
-
-db = sqlite3.connect(f)
-c = db.cursor()
-
 def getGrades(student):
-    q = "SELECT mark FROM students,courses WHERE students.id = courses.id and name=?"
-    sel = c.execute(q, (student,))
+    query = "SELECT mark FROM students,courses WHERE students.id = courses.id and name=?"
+    sel = cursor.execute(query, (student,))
     grades = []
     for record in sel:
         grades.append(record[0])
@@ -19,12 +14,14 @@ def getAvg(student):
      return mean(getGrades(student))
 
 def getAverages():
-    q = "SELECT name, id FROM students"
-    sel = c.execute(q)
+    sel = cursor.execute("SELECT name, id FROM students")
     students = []
     for record in sel:
         students.append(record)
     for student in students:
-        print "%s's (id: %d) average: \t%f"%(student[0],student[1],getAvg(student[0]))
+        print "%s's (id: %d) average: \t%f"%(student[0], student[1], getAvg(student[0]))
 
-getAverages()
+if __name__ == '__main__':
+    db = sqlite3.connect("discobandit.db")
+    cursor = db.cursor()
+    getAverages()
