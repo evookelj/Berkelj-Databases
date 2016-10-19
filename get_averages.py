79ef11de16b5acb09f3fd9ff0f2a4446be434c8e
Wilson Berkow <wilsonberkow@gmail.com>
2016-10-19 13:46:00 -0400

    Get student information by id instead of by name

--- get_averages.py
+++ get_averages.py
@@ -2,24 +2,24 @@ import sqlite3
 import csv
 from numpy import mean
 
-def getGrades(student):
-    query = "SELECT mark FROM students,courses WHERE students.id = courses.id and name=?"
-    sel = cursor.execute(query, (student,))
+def getGrades(student_id):
+    query = "SELECT mark FROM students,courses WHERE students.id = courses.id and students.id=?"
+    sel = cursor.execute(query, (student_id,))
     grades = []
     for record in sel:
         grades.append(record[0])
     return grades
 
-def getAvg(student):
-     return mean(getGrades(student))
+def getAvg(student_id):
+     return mean(getGrades(student_id))
 
 def getAverages():
     sel = cursor.execute("SELECT name, id FROM students")
     students = []
     for record in sel:
         students.append(record)
-    for student in students:
-        print "%s's (id: %d) average: \t%f"%(student[0], student[1], getAvg(student[0]))
+    for (name, id) in students:
+        print "%s's (id: %d) average: \t%f"%(name, id, getAvg(id))
 
 if __name__ == '__main__':
     db = sqlite3.connect("discobandit.db")
